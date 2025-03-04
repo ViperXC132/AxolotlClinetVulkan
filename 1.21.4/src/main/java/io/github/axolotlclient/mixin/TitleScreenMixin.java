276600ce6e2f1f18a73e94b3005cdc81d15a4c44
moehreag <moehreag@gmail.com>
2025-03-04 16:38:06 +0100

    update changelog

--- 1.21.4/src/main/java/io/github/axolotlclient/mixin/TitleScreenMixin.java
+++ 1.21.4/src/main/java/io/github/axolotlclient/mixin/TitleScreenMixin.java
@@ -46,16 +46,13 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -69,9 +66,6 @@ public abstract class TitleScreenMixin extends Screen {
 	@Shadow
 	@Nullable
 	private RealmsNotificationsScreen realmsNotificationsScreen;
-
-	@Unique
-	private static final WidgetSprites MUTE_BUTTON_SPRITES = new WidgetSprites(ResourceLocation.withDefaultNamespace("social_interactions/mute_button"), ResourceLocation.withDefaultNamespace("social_interactions/mute_button_highlighted"));
 
 	protected TitleScreenMixin() {
 		super(Component.empty());
