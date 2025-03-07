7326a15950d3584edf70f905776a18e209fb4f4b
moehreag <moehreag@gmail.com>
2025-03-07 18:34:21 +0100

    Reduce minimum size for keystrokes to 7x7

--- 1.20/src/main/java/io/github/axolotlclient/modules/hud/gui/keystrokes/ConfigureKeyBindScreen.java
+++ 1.20/src/main/java/io/github/axolotlclient/modules/hud/gui/keystrokes/ConfigureKeyBindScreen.java
@@ -50,8 +50,8 @@ public class ConfigureKeyBindScreen extends Screen {
 		this.hud = hud;
 		this.stroke = stroke;
 
-		width = new IntegerOption("", stroke.getBounds().width(), v -> stroke.getBounds().width(v), 10, 100);
-		height = new IntegerOption("", stroke.getBounds().height(), v -> stroke.getBounds().height(v), 10, 100);
+		width = new IntegerOption("", stroke.getBounds().width(), v -> stroke.getBounds().width(v), 7, 100);
+		height = new IntegerOption("", stroke.getBounds().height(), v -> stroke.getBounds().height(v), 7, 100);
 		this.isAddScreen = isAddScreen;
 	}
 
