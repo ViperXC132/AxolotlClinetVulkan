f53fd61b20140b7f0d32493ef31504d1eeb33df8
moehreag <moehreag@gmail.com>
2025-03-15 19:09:55 +0100

    fix reveal trap set off detection

--- 1.16_combat-6/src/main/java/io/github/axolotlclient/modules/hypixel/bedwars/upgrades/TrapUpgrade.java
+++ 1.16_combat-6/src/main/java/io/github/axolotlclient/modules/hypixel/bedwars/upgrades/TrapUpgrade.java
@@ -53,7 +53,7 @@ public class TrapUpgrade extends TeamUpgrade {
 
 	private final static Pattern[] REGEX = {
 		Pattern.compile("^\\b[A-Za-z0-9_§]{3,16}\\b purchased (.+) Trap.?\\s*$"),
-		Pattern.compile("Trap was set (off)!"),
+		Pattern.compile("[Tt]rap (?:was )?set (off)(?: by .+ from .+ team)?!"),
 		Pattern.compile("Removed (.+) from the (queue)!\\s*$")
 	};
 
