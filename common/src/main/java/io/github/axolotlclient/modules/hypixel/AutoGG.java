6288e5dbb7f795cbaeafed45da6d4d5440f1b3b2
moehreag <moehreag@gmail.com>
2025-09-07 15:19:46 +0200

    fix a bug

--- common/src/main/java/io/github/axolotlclient/modules/hypixel/AutoGG.java
+++ common/src/main/java/io/github/axolotlclient/modules/hypixel/AutoGG.java
@@ -159,7 +159,7 @@ public class AutoGG implements AbstractHypixelMod {
 
 	private void onMessage(ReceiveChatMessageEvent event) {
 		String message = event.getOriginalMessage();
-		if (client.br$isLocalServer()) {
+		if (!client.br$isLocalServer()) {
 			serverMap.keySet().forEach(s -> {
 				if (serverMap.get(s).get() && client.br$getServerAddress().contains(s)) {
 					if (gf.get()) {
