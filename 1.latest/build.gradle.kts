abe05baa090149316836cd6bf65b63ad2d55db66
moehreag <moehreag@gmail.com>
2025-09-23 23:36:46 +0200

    update to 1.21.9-pre4

--- 1.latest/build.gradle.kts
+++ 1.latest/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 	id("io.github.p03w.machete")
 }
 
-val minecraft = "1.21.9-pre2"
+val minecraft = "1.21.9-pre4"
 val minecraftFriendly = "1.21.9"
 val parchmentMinecraft = "1.21.8"
 val parchment = "2025.07.18"
 val modmenu = "14.0.0-rc.2"
-val fapi = "0.133.9"
+val fapi = "0.133.11"
 group = project.property("maven_group") as String
 version = "${project.property("version")}+$minecraftFriendly"
 base.archivesName = "AxolotlClient"
--- 1.latest/src/main/java/io/github/axolotlclient/mixin/ShieldSpecialRendererMixin.java
+++ 1.latest/src/main/java/io/github/axolotlclient/mixin/ShieldSpecialRendererMixin.java
@@ -37,8 +37,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ShieldSpecialRenderer.class)
 public abstract class ShieldSpecialRendererMixin {
 
-	@Inject(method = "submit(Lnet/minecraft/core/component/DataComponentMap;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;IIZ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ShieldModel;handle()Lnet/minecraft/client/model/geom/ModelPart;"))
-	private void axolotlclient$lowShield(DataComponentMap dataComponentMap, ItemDisplayContext itemDisplayContext, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int i, int j, boolean bl, CallbackInfo ci) {
+	@Inject(method = "submit(Lnet/minecraft/core/component/DataComponentMap;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;IIZI)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ShieldModel;handle()Lnet/minecraft/client/model/geom/ModelPart;"))
+	private void axolotlclient$lowShield(DataComponentMap dataComponentMap, ItemDisplayContext itemDisplayContext, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int i, int j, boolean bl, int k, CallbackInfo ci) {
 		if (AxolotlClient.config().lowShield.get()
 			&& Minecraft.getInstance().options.getCameraType().isFirstPerson()
 			&& (itemDisplayContext.equals(ItemDisplayContext.FIRST_PERSON_LEFT_HAND)
